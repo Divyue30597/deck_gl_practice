9c740226563e80f4a0ed62bbfdde2959ecd3f77d
Divyue30597 <tomchi.sharma305@outlook.com>
2024-04-25 11:09:20 +0530

    improved search

--- src/components/property/property.tsx
+++ src/components/property/property.tsx
@@ -25,27 +25,24 @@ export default function Property({ property, setId }: PropertyProps) {
       </div>
       <hr />
       <ul className={styles.property}>
-        {searchVal.length === 0
-          ? property.map((feat_property) => {
-              return (
-                <li key={feat_property.id}>
-                  <button onClick={() => setId(feat_property.id)}>
-                    {feat_property.Locality}
-                  </button>
-                  <p>{feat_property.city}</p>
-                </li>
-              );
-            })
-          : property.map((feat_property) => {
-              return searchVal.includes(feat_property.Locality) ? (
-                <li key={feat_property.id}>
-                  <button onClick={() => setId(feat_property.id)}>
-                    {feat_property.Locality}
-                  </button>
-                  <p>{feat_property.city}</p>
-                </li>
-              ) : null;
-            })}
+        {property.map((feat_property) => {
+          const isMatch =
+            searchVal.length === 0 ||
+            searchVal.includes(feat_property.Locality);
+
+          // Only render if there's a match
+          return (
+            <li
+              key={feat_property.id}
+              style={{ display: isMatch ? "flex" : "none" }}
+            >
+              <button onClick={() => setId(feat_property.id)}>
+                {feat_property.Locality}
+              </button>
+              <p>{feat_property.city}</p>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
